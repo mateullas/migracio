88749ed9fef3eafc869d7f3f651e3c53574daad6
mateullas <mateu.llas@udl.cat>
2020-03-23 16:41:18 +0100

    MSDINOU-30. Correcció d'un error amb l'exportació del DNI, GradebookNG

--- src/sakai/gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/importExport/ExportPanel.java
+++ src/sakai/gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/importExport/ExportPanel.java
@@ -300,7 +300,9 @@ public class ExportPanel extends BasePanel {
 					header.add(String.join(" ", IGNORE_COLUMN_PREFIX, getString("importExport.export.csv.headers.studentNumber")));
 				}
 				if (!isCustomExport || this.includeDNI) {
-					header.add(getString("importExport.export.csv.headers.DNI"));
+					header.add(String.format("%s%s",
+						CUSTOM_EXPORT_COLUMN_PREFIX,
+						getString("importExport.export.csv.headers.DNI")));
 				}
 
 				// get list of assignments. this allows us to build the columns and then fetch the grades for each student for each assignment from the map
